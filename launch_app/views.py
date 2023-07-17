f8e4c3777e2c82baaf369dea5370bf86b4ed0527
gaurav0720 <51204874+gaurav0720@users.noreply.github.com>
2023-07-17 18:02:19 -0500

    Update views.py

--- launch_app/views.py
+++ launch_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .emailHelper import EmailHelper
 from django.core.mail import send_mail
+from email.mime.text import MIMEText
 
 def index(request):
     if request.method == 'POST':
@@ -10,8 +11,8 @@ def index(request):
             contact_message = request.POST['contact_message']
 
             contact_message = "<strong>An user tried to contact you.</strong><br><br>These are the contact details:<br><strong>Name:</strong>{0}<br><strong>Email:</strong>{1}<br><strong>Message:</strong>{2}".format(contact_name, contact_email, contact_message)
-
-            send_mail('New user contacted at ADAPT Marketplace', contact_message, 'adaptmarketplace@gmail.com', ['ADAPTMarketplace@uabmc.edu', 'adaptmarketplace@gmail.com'], fail_silently=False)
+            mail = MIMEText(contact_message, 'html')
+            send_mail('New user contacted at ADAPT Marketplace', mail, 'adaptmarketplace@gmail.com', ['ADAPTMarketplace@uabmc.edu', 'adaptmarketplace@gmail.com'], fail_silently=False)
 
             EmailHelper.send_uab_message('New user contacted at ADAPT Marketplace', contact_message, ['ADAPTMarketplace@uabmc.edu'])
 
