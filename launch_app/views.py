83a12a3dfbe83c39f0bbb57b7f39b0da445b8dd9
gaurav0720 <51204874+gaurav0720@users.noreply.github.com>
2023-07-17 18:08:36 -0500

    Update views.py

--- launch_app/views.py
+++ launch_app/views.py
@@ -11,8 +11,7 @@ def index(request):
             contact_message = request.POST['contact_message']
 
             contact_message = "<strong>An user tried to contact you.</strong><br><br>These are the contact details:<br><strong>Name:</strong>{0}<br><strong>Email:</strong>{1}<br><strong>Message:</strong>{2}".format(contact_name, contact_email, contact_message)
-            mail = MIMEText(contact_message, 'html')
-            send_mail('New user contacted at ADAPT Marketplace', mail, 'adaptmarketplace@gmail.com', ['ADAPTMarketplace@uabmc.edu', 'adaptmarketplace@gmail.com'], fail_silently=False)
+            EmailHelper.send_uab_message_gmail('New user contacted at ADAPT Marketplace', contact_message, 'adaptmarketplace@gmail.com', ['ADAPTMarketplace@uabmc.edu', 'adaptmarketplace@gmail.com'], fail_silently=False)
 
             EmailHelper.send_uab_message('New user contacted at ADAPT Marketplace', contact_message, ['ADAPTMarketplace@uabmc.edu'])
 
