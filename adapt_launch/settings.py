38cf6921c9c5147ae8c0096999057969dd8b791b
gaurav0720 <51204874+gaurav0720@users.noreply.github.com>
2023-07-17 17:47:18 -0500

    Update settings.py

--- adapt_launch/settings.py
+++ adapt_launch/settings.py
@@ -128,6 +128,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'adaptmarketplace@gmail.com'
-EMAIL_HOST_PASSWORD = 'UAB2240Ero'
+EMAIL_HOST_PASSWORD = 'quyuhgrezsieoulu'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
