d9d84d6b7b5299202cfbc09bb2962cd409294750
gaurav0720 <gauravrathi241@outlook.com>
2023-07-17 17:12:14 -0500

    first commit

--- launch_app/views.py
+++ launch_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .emailHelper import EmailHelper
+# from .emailHelper import EmailHelper
+from django.core.mail import send_mail
 
 def index(request):
     if request.method == 'POST':
@@ -8,9 +9,9 @@ def index(request):
             contact_email = request.POST['contact_email']
             contact_message = request.POST['contact_message']
 
-            contact_message = "<strong>An user tried to contact you.</strong><br><br>These are the contact details:<br><strong>Name:</strong>{0}<br><strong>Email:</strong>{1}<br><strong>Message:</strong>{2}"
+            contact_message = "<strong>An user tried to contact you.</strong><br><br>These are the contact details:<br><strong>Name:</strong>{0}<br><strong>Email:</strong>{1}<br><strong>Message:</strong>{2}".format(contact_name, contact_email, contact_message)
 
-            EmailHelper.send_uab_message('New user contacted at ADAPT Marketplace', contact_message, ['ADAPTMarketplace@uabmc.edu'])
+            send_mail('New user contacted at ADAPT Marketplace', contact_message, 'adaptmarketplace@gmail.com', ['ADAPTMarketplace@uabmc.edu'], fail_silently=False)
 
             return render(request, 'index.html', {'contact_success': "success"})
     return render(request, 'index.html')
