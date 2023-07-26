a1da93a409b57f47d36502d1eb95de37401838c0
gaurav0720 <51204874+gaurav0720@users.noreply.github.com>
2023-07-26 17:55:35 -0500

    Update settings.py

--- adapt_launch/settings.py
+++ adapt_launch/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "launch_app/static",
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
